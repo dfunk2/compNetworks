28527cc766b0347ea97edfd41cbe42e17aa21d73
Denise <deltafoxtrot27@outlook.com>
2025-10-23 21:20:23 -0700

    add payload

--- Projects/P1_ClientServer/webserver.py
+++ Projects/P1_ClientServer/webserver.py
@@ -18,17 +18,15 @@ while True:
     #new socket is created but original is still listen, accept returns a tuple
     new_conn = serverSocket.accept()
     print("Client IP address and Port", new_conn) #prints my IP address and port number
-    #receive a new socket
+
     new_socket = new_conn[0]
 
     data = new_socket.recv(4096)
     clientRequest = data.decode("ISO-8859-1")
     print(clientRequest, end='')
 
-    for i in enumerate(clientRequest):
-        if clientRequest.endswith("\r\n\r\n"):
-            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\nConnection: close\r\n\r\n"
-            new_socket.sendall(response.encode("ISO-8859-1"))
-            #close the new socket and continue by accepting a new socket
-            new_socket.close()
+    if clientRequest.endswith("\r\n\r\n"):
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\nConnection: close\r\n\r\nhello!"
+        new_socket.sendall(response.encode("ISO-8859-1"))
+        new_socket.close()
     
